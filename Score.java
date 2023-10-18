03ca655b2231a87d7d1eac368cd349e9554e1bc9
H N D Y <119656030+hndypryg@users.noreply.github.com>
2023-10-18 22:44:28 +0800

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Score here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Score extends Actor
+{
+    /**
+     * Act - do whatever the Score wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public static int score;//membuat variabel score
+    public Score(){
+        score = 0;//pemberian nilai awal 0
+    }
+    public void act()
+    {
+        World myWorld = getWorld();
+        myWorld.showText(String.valueOf(score), 300, 100);//penempatan score
+    }
+    //score akan bertambah sesuai dengan pipa yang dilewati
+    public static void add(int num){
+        score += num;
+    }
+}
+
